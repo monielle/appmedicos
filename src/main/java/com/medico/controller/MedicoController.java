f63921a515bde49fed1101f37393b97777615ecb
monielle <monielle@gmail.com>
2023-02-17 14:44:53 -0300

    Created DTO DadosListagemMedico

--- src/main/java/com/medico/controller/MedicoController.java
+++ src/main/java/com/medico/controller/MedicoController.java
@@ -2,13 +2,14 @@ package com.medico.controller;
 
 
 import com.medico.medicos.DadosCadastroMedico;
+import com.medico.medicos.DadosListagemMedico;
 import com.medico.medicos.Medico;
 import com.medico.medicos.MedicoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -18,7 +19,14 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll();
+
     }
 }
---
+++ src/main/java/com/medico/medicos/DadosListagemMedico.java
@@ -0,0 +1,5 @@
+package com.medico.medicos;
+
+public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidadegi) {
+
+}
